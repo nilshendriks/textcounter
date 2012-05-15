4a206d0ec98954419c401d7a19b68c3e4bd7749d
Nils Hendriks <nils@nilshendriks.com>
2012-05-15 18:24:19 +0900

    initial staging of plugin file
    added textcounter.php file
    
    Signed-off-by: Nils Hendriks <nils@nilshendriks.com>

---
+++ textcounter.php
@@ -0,0 +1,50 @@
+<?php
+
+/*
+ * Text Counter
+ *
+ * A simple plugin that counts words and characters
+ * for any text
+ *
+ * Sample Usage:
+ *
+ * Word count
+ * <?php echo wordcount($page->text()) ?>
+ *
+ * Character count (incl spaces)
+ * <?php echo charcount($page->text()) ?>
+ *
+ * Character count no spaces
+ * <?php echo charcountnospaces($page->text()) ?>
+ *
+ * Author: Nirusu <http://nirusu.me>
+ * Contributor: Bastian Allgeier <http://getkirby.com>
+ *
+ */
+
+function wordcount($content) {
+  $words = kirbytext($content);
+  $wordcount = str_word_count(strip_tags($words));
+
+  return $wordcount;
+}
+
+function charcount($content) {
+  $words = kirbytext($content);
+  $words = strip_tags($words);
+  $words = trim($words);
+  $charcount = strlen($words);
+
+  return $charcount;
+}
+
+function charcountnospaces($content) {
+  $words = kirbytext($content);
+  $words = strip_tags($words);
+  $words = trim($words);
+
+  $words = ereg_replace('[[:space:]]+', '', $words);
+  $charcount = strlen($words);
+
+return $charcount;
+}
